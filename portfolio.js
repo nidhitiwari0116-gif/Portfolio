2e3e94e5b7e5525d4293e25627b63bd6e695ab19
nidhitiwari0116-gif <nidhitiwari0116@gmail.com>
2026-06-20 10:22:05 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById("resume");
+btn.addEventListener("click", function(){
+   window.open("Resume.pdf")
+}
+)
+document.getElementById("view project1");
+Prject.addEventListener("click", function(){
+    window.open("file:///C:/Users/Nidhi%20tiwari/Downloads/bakehub.html")
+}
+)
+document.getElementById("view project2");
+Prject.addEventListener("click", function(){
+    window.open("")
+}
+)
